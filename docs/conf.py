e5f27dcbaf94d42c38808140de82a7eedf6d050b
NickAlger <nalger225@gmail.com>
2026-04-05 10:42:26 -0500

    working on documentation website

--- docs/conf.py
+++ docs/conf.py
@@ -42,5 +42,7 @@ autoapi_own_page_level = 'function'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_favicon = 'favicon.ico'
+
 html_theme = 'agogo'
 html_static_path = ['_static']
---
+++ docs/modules.rst
@@ -0,0 +1,7 @@
+TuckerTensorTrainTools
+======================
+
+.. toctree::
+   :maxdepth: 4
+
+   t3tools
